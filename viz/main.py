dbf32182d2a2d7adc1c16ed921c5723e6e4ceabc
Allie Meng <allie@picwell.com>
2015-06-22 16:02:23 -0400

    done

--- viz/main.py
+++ viz/main.py
@@ -12,55 +12,49 @@ ec2 = boto.connect_ec2()
 cw = boto3.client('cloudwatch', 'us-east-1')
 
 
-zonea = "us-east-1a"
-zoneb = "us-east-1b"
-zoned = "us-east-1d"
-zonee = "us-east-1e"
+# # PRINT #######################################################################
+# #! List all the VPCs
+# def list_vpcs():
+#     vpclist = vpca.get_all_vpcs()
+#     print vpclist, len(vpclist)
 
 
-# PRINT #######################################################################
-#! List all the VPCs
-def list_vpcs():
-    vpclist = vpca.get_all_vpcs()
-    print vpclist, len(vpclist)
+# #! List the availability zones with the current region
+# def list_zones():
+#     zones = ec2.get_all_zones()
+#     print zones   
 
 
-#! List the availability zones with the current region
-def list_zones():
-    zones = ec2.get_all_zones()
-    print zones   
+# #! List the subnets
+# def list_subnets():
+#     snets = vpca.get_all_subnets()
+#     print snets, len(snets)
 
 
-#! List the subnets
-def list_subnets():
-    snets = vpca.get_all_subnets()
-    print snets, len(snets)
+# #! List the security groups the EC2 is a part of
+# def list_security_groups(ec2):
+#     sg = ec2.groups
+#     for g in sg:
+#          print g.name
+
+# ################################################################################
+# def find_used_zones():
+#     zlist = [find_subnets(zonea), find_subnets(zoneb), find_subnets(zonec),
+#     find_subnets(zoned)]
+#     for z in zlist:
+#         if len(z) == 0: zlist.remove(z)
+#     return zlist
 
 
-#! List the security groups the EC2 is a part of
-def list_security_groups(ec2):
-    sg = ec2.groups
-    for g in sg:
-         print g.name
+# def find_subnets_in_zones():
+#     return [find_subnets(zonea), find_subnets(zoneb), find_subnets(zoned),
+#     find_subnets(zonee)]
 
-################################################################################
-def find_used_zones():
-    zlist = [find_subnets(zonea), find_subnets(zoneb), find_subnets(zonec),
-    find_subnets(zoned)]
-    for z in zlist:
-        if len(z) == 0: zlist.remove(z)
-    return zlist
+# #! returns lists containing availability zones w/subnets, instances
+# def get_all_data():
+#     return find_subnets_in_zones() 
 
-
-def find_subnets_in_zones():
-    return [find_subnets(zonea), find_subnets(zoneb), find_subnets(zoned),
-    find_subnets(zonee)]
-
-#! returns lists containing availability zones w/subnets, instances
-def get_all_data():
-    return find_subnets_in_zones() 
-
-################################################################################
+# ################################################################################
 
 #! Return the subnets in the vpc and zone
 def subnets_in_zone_vpc(zone, vpc):
@@ -74,11 +68,6 @@ def subnets_in_zone(zone):
 # ! Return the instances in a subnet
 def instances_in_subnet(snet):
     return ec2.get_only_instances(filters = {"subnet-id" : snet})
-
-# def instances_in_subnet(snetname):
-#     return ec2.get_only_instances(filters = {"tag:key=value" : })
-
-#print ec2.get_only_instances()[0].tags
 
 def sg_name(sg):
     return str(sg.name)
@@ -95,18 +84,18 @@ def instance_name_sg(inst):
 
 #! Return the all the data about one vpc in format (this is without security 
 #  groups) : 
-def vpc_data(vpc):
-    overall = []
-    for i,z in enumerate(ec2.get_all_zones()):
-        overall.append([str(z.name)])
-        snets = subnets_in_zone_vpc(z.name,vpc)
-        #print z.name + ": ", len(snets), snets
-        for j,s in enumerate(snets, start =1):   
-            #print str(s.tags['Name'])    
-            overall[i].append([str(s.tags['Name'])])
-            instnamelist = map(instance_name, instances_in_subnet(s.id))
-            overall[i][j].append(instnamelist)
-    return list(filter((lambda x: len(x) > 1),overall))
+# def vpc_data(vpc):
+#     overall = []
+#     for i,z in enumerate(ec2.get_all_zones()):
+#         overall.append([str(z.name)])
+#         snets = subnets_in_zone_vpc(z.name,vpc)
+#         #print z.name + ": ", len(snets), snets
+#         for j,s in enumerate(snets, start =1):   
+#             #print str(s.tags['Name'])    
+#             overall[i].append([str(s.tags['Name'])])
+#             instnamelist = map(instance_name, instances_in_subnet(s.id))
+#             overall[i][j].append(instnamelist)
+#     return list(filter((lambda x: len(x) > 1),overall))
 
 def fix_subnet_order(data):
     az2newlyordered = [data[1][0]]
@@ -133,18 +122,18 @@ def vpc_data_sg_order(vpc):
     return fix_subnet_order(overall)
 
 #! Same as vpc_data but also has security group relations
-def vpc_data_sg(vpc):
-    overall = []
-    f = []
-    for i,z in enumerate(ec2.get_all_zones()):
-        overall.append([str(z.name)])
-        snets = subnets_in_zone_vpc(z.name,vpc)
-        #print z.name + ": ", len(snets), snets
-        for j,s in enumerate(snets, start =1):     
-            overall[i].append([str(s.tags['Name'])])
-            instnamelist = map(instance_name_sg, instances_in_subnet(s.id))
-            overall[i][j].append(instnamelist)
-    return list(filter((lambda x: len(x) > 1),overall))
+# def vpc_data_sg(vpc):
+#     overall = []
+#     f = []
+#     for i,z in enumerate(ec2.get_all_zones()):
+#         overall.append([str(z.name)])
+#         snets = subnets_in_zone_vpc(z.name,vpc)
+#         #print z.name + ": ", len(snets), snets
+#         for j,s in enumerate(snets, start =1):     
+#             overall[i].append([str(s.tags['Name'])])
+#             instnamelist = map(instance_name_sg, instances_in_subnet(s.id))
+#             overall[i][j].append(instnamelist)
+#     return list(filter((lambda x: len(x) > 1),overall))
     # print overall
     # return fix_subnet_order(overall)
 
@@ -163,11 +152,6 @@ def vpc_data_sg(vpc):
 #             overall[0][j].append(instnamelist)
 #     return list(filter((lambda x: len(x) > 1),overall))
 
-vpc_data_sg("vpc-8ada78ef")
-
-
-# print subnets_in_zone_vpc(zoneb, "vpc-1d39cf78")
-# print subnets_in_zone(zoneb)
 
 ################################################################################
 #TESTING
@@ -177,14 +161,9 @@ vpc_data_sg("vpc-8ada78ef")
 def find_subnets(vpc):
     return vpca.get_all_subnets(filters = {"vpc-id" : vpc})
 
-
-
-
 #print find_subnets("vpc-1d39cf78")
 #print zones_in_vpc("available")
 #print instances_in_subnet("subnet-65e7233c")[0].tags
-
-
 
 #TAGS
 def instances():
@@ -200,29 +179,25 @@ def instances():
 # u'Purpose': u'sliders', u'aws:autoscaling:groupName': u'awseb-e-dkiwqfqktn-stack-AWSEBAutoScalingGroup-E3D0MLXAU1EM', 
 # u'Name': u'predictions-staging'}
 
-mlst = ["hi", ["a","b","c"]]
-slst = ["asdf", "fghi"]
-lst = [1,2,3]
-
 @app.route('/')
 def default():
-    return render_template('test.html')
+    return render_template('index.html', data = vpc_data_sg_order("vpc-8ada78ef"))
 
 @app.route('/databricks')
 def databricks():
-    return render_template('mvp.html', data = vpc_data_sg_order("vpc-6d5fd208"))
+    return render_template('index.html', data = vpc_data_sg_order("vpc-6d5fd208"))
 
 @app.route('/staging')
 def staging():
-    return render_template('mvp.html', data = vpc_data_sg_order("vpc-1d39cf78"))
+    return render_template('index.html', data = vpc_data_sg_order("vpc-1d39cf78"))
 
 @app.route('/uat')
 def uat():
-    return render_template('mvp.html', data = vpc_data_sg_order("vpc-ba0cc7df"))
+    return render_template('index.html', data = vpc_data_sg_order("vpc-ba0cc7df"))
 
 @app.route('/production')
 def production():
-    return render_template('mvp.html', data = vpc_data_sg_order("vpc-8ada78ef"))
+    return render_template('index.html', data = vpc_data_sg_order("vpc-8ada78ef"))
 
 
 # [VPC:vpc-6d5fd208; databricks, VPC:vpc-1d39cf78; staging, VPC:vpc-ba0cc7df; uat, VPC:vpc-8ada78ef; production] 
